dd1c23dd8e095470df04522eccb9de9f0a4a20fd
PawanKuma <pwnkmr.dst@gmail.com>
2024-06-13 18:58:19 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,3 +1,6 @@
-function myFunction(req, res) {
-  res.send('Skills')
+function calculateNumbers() {
+  const number1 = 10
+  const number2 = 20
+  const sum = number1 + number2
+  console.log(sum)
 }
